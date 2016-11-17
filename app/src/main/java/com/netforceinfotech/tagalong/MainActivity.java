f78e300f9e07f72261d5b7925364590b72b7d5d9
kunsang wangyal <kunwangyal05@gmail.com>
2016-11-17 18:32:00 +0530

    b activity created

--- app/src/main/java/com/netforceinfotech/tagalong/MainActivity.java
+++ app/src/main/java/com/netforceinfotech/tagalong/MainActivity.java
@@ -1,13 +1,27 @@
 package com.netforceinfotech.tagalong;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.button).setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.button:
+                Intent intent=new Intent(this,BActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
